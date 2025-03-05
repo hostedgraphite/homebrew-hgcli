7b8043afe866b3441988fb78670d87179d7e2dd3
MetricFire <support@metricfire.com>
2025-03-05 15:44:25 +0900

    Brew formula update for hg-cli version v0.0.1

--- Formula/hg-cli.rb
+++ Formula/hg-cli.rb
@@ -5,11 +5,11 @@
 class HgCli < Formula
   desc ""
   homepage "https://github.com/hostedgraphite/homebrew-hgcli"
-  version "0.0.1-alpha"
+  version "0.0.1"
 
   on_macos do
-    url "https://github.com/hostedgraphite/hg-cli/releases/download/v0.0.1-alpha/hg-cli_0.0.1-alpha_darwin_all.tar.gz"
-    sha256 "38d69ec9c12e056f5019709e9f9e81e917f694e700bf9c5fb09d958196b08b96"
+    url "https://github.com/hostedgraphite/hg-cli/releases/download/v0.0.1/hg-cli_0.0.1_darwin_all.tar.gz"
+    sha256 "4c983b6699ea77c3a5ea5d214a7648f8907e23f85cf6a08abe8f3f8d6a1d0974"
 
     def install
       bin.install "hg-cli"
@@ -19,8 +19,8 @@ class HgCli < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/hostedgraphite/hg-cli/releases/download/v0.0.1-alpha/hg-cli_0.0.1-alpha_linux_amd64.tar.gz"
-        sha256 "53ae2872b83984e5dbb22b896b985641bd4826847f69317550ca5f1ae43dab03"
+        url "https://github.com/hostedgraphite/hg-cli/releases/download/v0.0.1/hg-cli_0.0.1_linux_amd64.tar.gz"
+        sha256 "025a97c404f903108ed7786704d4cf1c7e6c5d78e630b1c194ae6fdf0d0b719f"
 
         def install
           bin.install "hg-cli"
@@ -29,8 +29,8 @@ class HgCli < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/hostedgraphite/hg-cli/releases/download/v0.0.1-alpha/hg-cli_0.0.1-alpha_linux_arm64.tar.gz"
-        sha256 "3209b0ef14b1aec2fd99396c1e46499b88354b484377537201712f88efd5f356"
+        url "https://github.com/hostedgraphite/hg-cli/releases/download/v0.0.1/hg-cli_0.0.1_linux_arm64.tar.gz"
+        sha256 "bb4c7a77b29a517dd37b98092acc1773154061e2bc9b3abb0ae4bb5a3586f751"
 
         def install
           bin.install "hg-cli"
